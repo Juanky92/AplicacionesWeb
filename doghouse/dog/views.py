2861381e6b182881aca6239ca83c760e91191a93
Juan Carlos <jcbarrientos1992@gmail.com>
2017-10-16 18:17:08 +0200

    Decimo

--- doghouse/dog/views.py
+++ doghouse/dog/views.py
@@ -11,51 +11,59 @@ def inicio(request):
 class perroCreate(CreateView):
 	model=Perro
 	fields=['microchip','nombre','edad','caracter','habitos','peso','vacunas','color','uso','altura','raza','cruce','descripcion','estado']
+	success_url = reverse_lazy("Lista-de-perros")
 
 class usuarioCreate(CreateView):
 	model=Usuario
 	fields =['nombre','apellidos','dni','sexo','telefono','edad','domicilio','cp','localidad','correo']
+	success_url = reverse_lazy("Lista-de-usuarios")
 
 class razaCreate(CreateView):
 	model=Raza
 	fields =['raza']
+	success_url = reverse_lazy("Lista-de-razas")
 
 class propiedadeCreate(CreateView):
 	model=Propiedade
 	fields =['perro','usuario']
+	success_url = reverse_lazy("Lista-de-propiedad")
 
 
 class perroUpdate(UpdateView):
 	model=Perro
 	fields=['microchip','nombre','edad','caracter','habitos','peso','vacunas','color','uso','altura','raza','cruce','descripcion','estado']
+	success_url = reverse_lazy("Lista-de-perros")
 
 class usuarioUpdate(UpdateView):
 	model=Usuario
 	fields =['nombre','apellidos','dni','sexo','telefono','edad','domicilio','cp','localidad','correo']
+	success_url = reverse_lazy("Lista-de-usuarios")
 
 class razaUpdate(UpdateView):
 	model=Raza
 	fields =['raza']
+	success_url = reverse_lazy("Lista-de-razas")
 
 class propiedadeUpdate(UpdateView):
 	model=Propiedade
 	fields =['perro','usuario']
+	success_url = reverse_lazy("Lista-de-propiedad")
 
 class perroDelete(DeleteView):
 	model=Perro
-	success_url = reverse_lazy('perro_list')
+	success_url = reverse_lazy("Lista-de-perros")
 
 class usuarioDelete(DeleteView):
 	model=Usuario
-	success_url = reverse_lazy('usuario_list')
+	success_url = reverse_lazy("Lista-de-usuarios")
 
 class razaDelete(DeleteView):
 	model=Raza
-	success_url = reverse_lazy('raza_list')
+	success_url = reverse_lazy("Lista-de-razas")
 
 class propiedadeDelete(DeleteView):
 	model=Propiedade
-	success_url = reverse_lazy('propiedade_list')
+	success_url = reverse_lazy("Lista-de-propiedad")
 
 
 class perroList(ListView):
@@ -95,42 +103,3 @@ class razaDetail(DetailView):
 class propiedadeDetail(DetailView):
 	queryset=Propiedade.objects.all()
 
-#def usuario(request):
-	#form=UsuarioForm(request.POST or None)
-	#if form.is_valid():
-		#form_data=form.cleaned_data
-		#abc=form_data.get('nombre')
-		#abc2=form_data.get('apellidos')
-		#abc3=form_data.get('dni')
-		#abc4=form_data.get('sexo')
-		#abc5=form_data.get('telefono')
-		#abc6=form_data.get('edad')
-		#abc7=form_data.get('domicilio')
-		#abc8=form_data.get('cp')
-		#abc9=form_data.get('localidad')
-		#abc10=form_data.get('correo')
-		#obj=Usuario.objects.create(nombre=abc,apellidos=abc2,dni=abc3,sexo=abc4,telefono=abc5,edad=abc6,domicilio=abc7,cp=abc8,localidad=abc9,correo=abc10)
-	#context={"el_form":form,}
-	#return render(request,"usuario.html",context)
-
-#def perro(request):
-	#form=PerroForm(request.POST or None)
-	#if form.is_valid():
-		#form_data=form.cleaned_data
-		#abc=form_data.get('microchip')
-		#abc2=form_data.get('nombre')
-		#abc3=form_data.get('edad')
-		#abc4=form_data.get('caracter')
-		#abc5=form_data.get('habitos')
-		#abc6=form_data.get('peso')
-		#abc7=form_data.get('vacunas')
-		#abc8=form_data.get('color')
-		#abc9=form_data.get('uso')
-		#abc10=form_data.get('altura')
-		#abc11=form_data.get('raza')
-		#abc12=form_data.get('cruze')
-		#abc13=form_data.get('descripcion')
-		#abc14=form_data.get('estado')
-		#obj=Perro.objects.create(microchip=abc,nombre=abc2,edad=abc3,caracter=abc4,habitos=abc5,peso=abc6,vacunas=abc7,color=abc8,uso=abc9,altura=abc10,raza=abc11,cruze=abc12,descripcion=abc13,estado=abc14)
-	#context={"ele_form":form,}
-	#return render(request,"perro.html",context)
--- doghouse/doghouse/urls.py
+++ doghouse/doghouse/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^dog/', include('dog.urls')),
-    url(r'^$', RedirectView.as_view(url='/dog/inicio')),
+    url(r'^admin/',admin.site.urls),
+    url(r'^dog/',include('dog.urls')),
+    url(r'^$',RedirectView.as_view(url='/dog/inicio')),
 ]
