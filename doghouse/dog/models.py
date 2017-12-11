442cccfd8626ab6b48c322bd957fe9fd176f07e7
Juan Carlos <jcbarrientos1992@gmail.com>
2017-12-11 05:40:55 +0100

    VigesimoSegundo

--- doghouse/dog/models.py
+++ doghouse/dog/models.py
@@ -1,12 +1,14 @@
 from __future__ import unicode_literals
 from django.urls import reverse
 from django.db import models
+from django.contrib import auth
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
 import os
-# # Create your models here.
+
+
 def path_and_renames(instance, filename):
 	upload_to = settings.MEDIA_ROOT + "/razas"
 	ext = filename.split('.')[-1]
@@ -16,16 +18,6 @@ def path_and_renames(instance, filename):
 	# return the whole path to the file
 	return os.path.join(upload_to, filename)
 
-def path_and_renamese(instance, filename):
-	upload_to = settings.MEDIA_ROOT
-	ext = filename.split('.')[-1]
-	# get filename
-	if instance.pk:
-		filename = '{}.{}'.format(instance.pk, "jpg")
-	# return the whole path to the file
-	return os.path.join(upload_to, filename)
-
-
 class Raza(models.Model):
 	raza=models.CharField(max_length=150)
 	descripcion=models.TextField(max_length=5000, null=True)
@@ -34,6 +26,15 @@ class Raza(models.Model):
 		return self.raza
 	def get_absolute_url(self):
 		return reverse('raza_detail',kwargs={'pk':self.pk})
+	# Override del save() por defecto.
+	def save(self, *args, **kwargs):
+		if self.pk is None:
+			foto = self.imagen
+			self.imagen = None
+			super(Raza, self).save(*args, **kwargs)
+			self.imagen = foto
+			kwargs.pop('force_insert')
+		super(Raza, self).save(*args, **kwargs)
 
 
 class Usuario(models.Model):
@@ -65,6 +66,12 @@ class Usuario(models.Model):
 	def get_absolute_url(self):
 		return reverse('usuario_detail',kwargs={'pk':self.pk})
 
+def path_and_renamese(instance, filename):
+	upload_to = settings.MEDIA_ROOT
+	ext = filename.split('.')[-1]
+	if instance.pk:
+		filename = '{}.{}'.format(instance.pk, "jpg")
+	return os.path.join(upload_to, filename)
 
 class Perro(models.Model):
 	adoptable='A'
@@ -75,7 +82,6 @@ class Perro(models.Model):
 	microchip=models.CharField(max_length=30,unique=True)
 	nombre=models.CharField(max_length=100)
 	edad=models.IntegerField()
-	foto=models.ImageField(upload_to=path_and_renamese, null=True)
 	caracter=models.CharField(max_length=50)
 	habitos=models.CharField(max_length=150)
 	peso=models.IntegerField()
@@ -90,6 +96,17 @@ class Perro(models.Model):
 	estado_choices=(('A','adoptable'),('AD','adoptado'),)
 	estado=models.CharField(max_length=2,choices=estado_choices,default=adoptable,)
 	propietario=models.ForeignKey(User)
+	foto=models.ImageField(upload_to=path_and_renamese, null=True)
+
+	# Override del save() por defecto.
+	def save(self, *args, **kwargs):
+		if self.pk is None:
+			imagen = self.foto
+			self.foto = None
+			super(Perro, self).save(*args, **kwargs)
+			self.foto = imagen
+			kwargs.pop('force_insert')
+		super(Perro, self).save(*args, **kwargs)
 	
 
 	def __unicode__(self):
